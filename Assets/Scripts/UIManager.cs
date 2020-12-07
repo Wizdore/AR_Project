47fd8c348c9b648f3536c113b844f6871d60d78a
Wizdore <11255780+Wizdore@users.noreply.github.com>
2020-12-07 04:52:20 +0100

    Huge UI overhaul
    Added asset inventory ui complete with scrollable list of assets. and a lot more.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using  DG.Tweening;
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private RectTransform assetPanel, createButton;
+
+    public void ShowCreateButton()
+    {
+        createButton
+            .DOAnchorPosX(-50f, 0.3f)
+            .SetEase(Ease.OutElastic);
+    }
+    
+    public void HideCreateButton()
+    {
+        createButton
+            .DOAnchorPosX(160f, 0.3f)
+            .SetEase(Ease.InElastic);
+    }
+    
+    public void ShowAssetPanel()
+    {
+        assetPanel
+            .DOAnchorPosY(-130f, 0.6f)
+            .SetEase(Ease.OutElastic);
+    }
+    public void HideAssetPanel()
+    {
+        assetPanel
+            .DOAnchorPosY(-550f, 0.6f)
+            .SetEase(Ease.InElastic);
+    }
+}
