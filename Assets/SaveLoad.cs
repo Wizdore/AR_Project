fa539d98ad5130c41793779c07ddb9537d2487ac
Wizdore <11255780+Wizdore@users.noreply.github.com>
2020-12-21 22:52:57 +0100

    SDF based text added in input field of saving system

---
+++ Assets/SaveLoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class SaveLoad : MonoBehaviour
+{
+    [SerializeField] private GameObject gameObjectToSave;
+    [SerializeField] private TMP_InputField savenameInputField;
+
+    public void Save()
+    {
+        string savename = savenameInputField.GetComponent<TMP_InputField>().text;
+    }
+}
